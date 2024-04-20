a135571a6380f71aadc318ce28f0676c001b0ff6
Zhengnan Lai <zhengnan@login07.chn.perlmutter.nersc.gov>
2024-04-20 07:21:49 -0700

    naive LU

---
+++ serial.cpp
@@ -0,0 +1,22 @@
+const char* lu_desc = "Naive LU decomposition.";
+
+/*
+ * This routine performs a LU decomposition
+ *  A = L * U
+ * where A, L, and U are lda-by-lda matrices stored in row-major format.
+ * On exit, A maintain its input values.
+ */
+void lu_decomposition(int n, double* A, double* L, double* U) {
+    for(int i = 0; i < n; i++) for(int j = i; j < n; j++){
+        U[i * n + j] = A[i * n + j];
+    }
+    for(int k = 0; k < n; k++){
+        L[k * n + k] = 1;
+        for(int i = k + 1; i < n; i++){
+            L[i * n + k] = A[i * n + k] / A[k * n + k];
+        }
+        for(int j = k + 1; j < n; j++) for(int i = k + 1; i < n; i++){
+            U[i * n + j] = U[i * n + j] - L[i * n + k] * U[k * n + j];
+        }
+    }
+}
