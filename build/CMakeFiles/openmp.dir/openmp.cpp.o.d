958cda01a9109a6b0bfddb56265b24925884633d
Zhengnan Lai <zhengnan@x1204c0s3b0n0h0.chn.perlmutter.nersc.gov>
2024-04-29 12:15:01 -0700

    remove build and create gitignore

--- build/CMakeFiles/openmp.dir/openmp.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/openmp.dir/openmp.cpp.o: \
- /global/homes/z/zhengnan/HPC_LUDecomposition/openmp.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/serial.dir/serial.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/serial.dir/serial.cpp.o: \
- /global/homes/z/zhengnan/HPC_LUDecomposition/serial.cpp \
- /usr/include/stdc-predef.h
